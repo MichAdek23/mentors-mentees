46e50af51b7515725768159dbeeead58f75e24a0
Yun4g <delightvincent487@gmail.com>
2025-05-06 12:40:26 +0100

    push tes dashboard

--- src/component/MeentoDashboard/mentor-DashBoard.jsx
+++ src/component/MeentoDashboard/mentor-DashBoard.jsx
@@ -27,22 +27,17 @@ function MentorDashBoard() {
     const { ActiveComponent, toggleState } = context;
 
     return (
-        <main className="flex h-full bg-white">
-     
-        <div className={`lg:hidden fixed inset-0 z-50  bg-white transform transition-transform duration-300 ${toggleState ? 'translate-x-0' : 'translate-x-full'}`}>
-          <NavRes />
-        </div>
-
-        <aside className="hidden lg:block h-screen w-[18%] bg-white">
-          <NavBarDashboard />
-        </aside>
-      
-
-        <section className="flex-1 h-fit bg-slate-100 py-10 px-4 md:px-10 lg:px-[34px]">
-          <ActiveComponent />
-        </section>
-      </main>
-      
+         <main className='flex bg-white sidebar h-full'>
+            <div className={`${toggleState ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 fixed h-screen lg:hidden z-50 top-0 bg-white w-full`}>
+                <NavRes/>
+            </div>
+            <section className='hidden lg:block h-screen w-[18%]'>
+                <NavBarDashboard/>
+            </section>
+            <section className='h-fit sidebar md:px-10 lg:px-[34px] bg-slate-100 py-10 w-full'>
+                <ActiveComponent/> 
+            </section> 
+         </main>
     );
 }
 
