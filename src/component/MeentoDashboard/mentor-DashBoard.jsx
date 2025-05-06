58934f1b6139142625d75e25226d4965d2a9f6c2
Yun4g <delightvincent487@gmail.com>
2025-05-06 12:46:32 +0100

    test navbar

--- src/component/MeentoDashboard/mentor-DashBoard.jsx
+++ src/component/MeentoDashboard/mentor-DashBoard.jsx
@@ -28,7 +28,7 @@ function MentorDashBoard() {
 
     return (
          <main className='flex bg-white sidebar h-full'>
-            <div className={`${toggleState ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 fixed h-screen lg:hidden z-50 top-0 bg-white w-full`}>
+            <div className={`${toggleState ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 fixed h-screen lg:hidden z-50 top-0  w-full`}>
                 <NavRes/>
             </div>
             <section className='hidden lg:block h-screen w-[18%]'>
